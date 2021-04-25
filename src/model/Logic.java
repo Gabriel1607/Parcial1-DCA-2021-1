d94202c87b7b257a88461f2b5b85eebf4105e835
Gabriel <gabrielmora2001@hotmail.com>
2021-04-25 11:04:21 -0500

    Leer TXT (aun no funciona)

--- src/model/Logic.java
+++ src/model/Logic.java
@@ -1,10 +1,23 @@
 package model;
 
+import java.util.LinkedList;
+
 import processing.core.PApplet;
 
 public class Logic {
 	private PApplet app;
+	private String[] info1;
+	private String[] info2;
+	private LinkedList<Dog> ListOfDogs;
 	public Logic(PApplet app) {
 		this.app = app;
+		info1 = app.loadStrings("../texts/TXT1.txt");
+		info2 = app.loadStrings("../texts/TXT2.txt");
+		ListOfDogs = new LinkedList<Dog>();
+	}
+	public void TextDogs() {
+		for (int i = 0; i < info1.length; i++) {
+			
+		}
 	}
 }
